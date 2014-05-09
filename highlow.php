b5ac5e5e39676885add84f516113afff83aee819
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-09 13:28:13 -0500

    Added 'Say' functionality.

--- highlow.php
+++ highlow.php
@@ -3,18 +3,28 @@
 	$number = rand(1, 100);
 	//echo "$number\n";
 
-	fwrite(STDOUT, "Hello!  What is your name?\n");
-	$name = fgets(STDIN);
+	fwrite(STDOUT, "Hello!  What is your name?\n") && 
+		exec('say "Hello!  What is your name?"');
+		$name = fgets(STDIN);
 
-	fwrite(STDOUT, "Hey there, $name");
-	fwrite(STDOUT, "Think you can guess my number??\n");
+	fwrite(STDOUT, "Hey there, $name") && exec('say "Hey there, $name"');
+	fwrite(STDOUT, "Think you can guess my number?\n") && exec('say "Think you can guess my number?"');
 
 	$guess = fgets(STDIN);
 
 while ($guess != $number) { 
 
-		if ($guess < $number) { fwrite(STDOUT, "Higher..."); $guess = fgets(STDIN); } 
-		elseif ($guess > $number) { fwrite(STDOUT, "Lower..."); $guess = fgets(STDIN); }
+	if ($guess < $number) { 
+		fwrite(STDOUT, "Higher...") && exec('say "Higher!"'); 
+			  $guess = fgets(STDIN); 
+			} 
+		elseif ($guess > $number) { 
+			fwrite(STDOUT, "Lower...") && exec('say "Lower!"'); 
+				  $guess = fgets(STDIN); 
+				}
 }
 
-if ($guess == $number) { fwrite(STDOUT, "Wow!  What a guesser you are.  Good job!\n"); }
+if ($guess == $number) { 
+	fwrite(STDOUT, "Wow!  What a guesser you are.  Good job!\n") &&
+		exec('say "Wow!  What a guesser you are.  Good job!"'); 
+	}
