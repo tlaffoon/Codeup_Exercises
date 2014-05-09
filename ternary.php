3a89baef7d24392ca2f24f0cadcc90e935153f93
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-09 13:45:16 -0500

    Added intial ternary.php

---
+++ ternary.php
@@ -0,0 +1,21 @@
+<?php
+
+$age = '17';
+$is_adult = $age >= 18 ? true : false;
+
+var_dump($is_adult);
+
+$a = 5;
+$b = 10;
+
+if ($a == $b) {
+    echo "$a is equal to $b";
+} else {
+    echo "$a is not equal to $b";
+}
+
+
+$a = 5;
+$b = 10;
+
+echo ($a == $b) ? "$a is equal to $b" : "$a is not equal to $b";
