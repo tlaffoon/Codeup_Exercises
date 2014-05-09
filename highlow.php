8ab2bd920c924cf71dc90310f8519756c0bcbf01
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-09 14:51:51 -0500

    Formatting.

--- highlow.php
+++ highlow.php
@@ -8,8 +8,7 @@
 // -------------------------------------------------- //
 
 // Prompt for user name.
-	fwrite(STDOUT, "Hello!  What is your name?\n") && 
-		exec('say "Hello!  What is your name?"');
+	fwrite(STDOUT, "Hello!  What is your name?\n") && exec('say "Hello!  What is your name?"');
 
 // Get user name from user input.
 	$name = fgets(STDIN);
@@ -41,13 +40,17 @@
 // Check to see if guess is higher than number, then prompt.
 	if ($guess < $number) { 
 		fwrite(STDOUT, "Higher...") && exec('say "Higher!"'); 
-			$guess = fgets(STDIN); 
+		
+		// Prompt user for another guess.
+		$guess = fgets(STDIN); 
 	} 
 
 // Check to see if guess is lower than number, then prompt.
 	elseif ($guess > $number) { 
 		fwrite(STDOUT, "Lower...") && exec('say "Lower!"'); 
-			$guess = fgets(STDIN); 
+		
+		// Prompt user for another guess.
+		$guess = fgets(STDIN); 
 	}
 }
 
@@ -56,7 +59,7 @@
 // Once the guess matches the number, the main while loop exits and this code runs.
 if ($guess == $number) { 
 	fwrite(STDOUT, "Wow!  You guessed my number in $count tries.  Good job!\n") &&
-		exec('say "Wow!  What a guesser you are!  Good job!"'); 
+	exec('say "Wow!  What a guesser you are!  Good job!"'); 
 	}
 
 // -------------------------------------------------- //
