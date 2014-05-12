d4758197a72915d0b4801802bfcd87b0b4c73592
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-12 17:51:23 -0500

    Altered increment variable to allow user to set.

--- for.php
+++ for.php
@@ -7,9 +7,12 @@ $n1 = (int) fgets(STDIN);
 fwrite(STDOUT, 'Please input your ending number: ' . PHP_EOL) ;
 $n2 = (int) fgets(STDIN);
 
-if (is_numeric($n1) && is_numeric($n2)) {
+fwrite(STDOUT, 'Please choose your integer of increment: ' . PHP_EOL) ;
+$n3 = (int) fgets(STDIN);
 
-		for ($x=$n1; $x<=$n2; $x++) {
-			echo "\$x is equal to {$x}\n";
-		}
+if (is_numeric($n1) && is_numeric($n2) && is_numeric($n3)) {
+
+	for ($x=$n1; $x<=$n2; $x+=$n3) {
+		echo "\$x is equal to {$x}\n" ;
 	}
+}
