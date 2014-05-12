976f88137e11e9acaaf202e7bf4a17751feb7032
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-12 09:17:16 -0500

    Completed if_and_or.php exercise.

--- if_and_or.php
+++ if_and_or.php
@@ -27,12 +27,3 @@ if ($x > 0 && $x < 10) { echo "$x is greater than 0 AND less than 10\n"; }
 if ($y > 0 && $y < 10) { echo "$y is greater than 0 AND less than 10\n"; }
 
 if ($z > 0 && $z < 10) { echo "$z is greater than 0 AND less than 10\n"; }
-
-
-/*
-0 < 5 < 10
-0 is greater than 0 OR less than 10
-5 is greater than 0 OR less than 10
-10 is greater than 0 OR less than 10
-5 is greater than 0 AND less than 10
-*/
