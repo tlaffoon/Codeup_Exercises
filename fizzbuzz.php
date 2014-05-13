73ae3f6b540ddc01b03a7be736c5948934af94ba
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-13 08:39:24 -0500

    Added additional logic and prompts for user input.  Added occurence count.

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,11 +1,26 @@
 <?php
+		fwrite(STDOUT, 'Please enter a minimum, then maximum number: ' . PHP_EOL);
+		$min = (int) fgets(STDIN);
+		$max = (int) fgets(STDIN);
 
-		for ($x=0; $x<=100; $x+=1) {
+		for ($x=$min; $x<=$max; $x+=1) {
+			
 			if (($x % 3) == 0 && ($x % 5) == 0) {
 				echo "FizzBuzz" . PHP_EOL;
+				$fbcount++;
+
 			} elseif (($x % 3) == 0) {
 				echo "Fizz" . PHP_EOL;
+				$fcount++;
+
 			} elseif (($x % 5) == 0) {
 				echo "Buzz" . PHP_EOL;
+				$bcount++;
+
 			} else echo "{$x}" . PHP_EOL ;
 		}
+
+		echo "\n";
+		echo "\"Fizz\" - {$fcount} occurrences.\n";
+		echo "\"Buzz\" - {$bcount} occurrences.\n";
+		echo "\"FizzBuzz\" - {$fbcount} occurrences."  . PHP_EOL ;
