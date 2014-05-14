8a9b90f96b8d997a9060b5ee1d4f7dba9457d7bf
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-14 10:15:41 -0500

    Added break_continue.php exercise.

---
+++ break_continue.php
@@ -0,0 +1,18 @@
+<?php
+
+// Label even numbers and continue the loop.
+for ($num = 0; $num <= 100; $num++) {
+    echo $num . "\n";
+    if (($num % 2)) {
+        continue;
+    }
+    echo "^ that is an even number.\n";
+}
+
+// Break after hitting 10.
+for ($num = 0; $num <= 100; $num++) {
+    echo $num . "\n";
+    if (($num == 10)) {
+        break;
+    }
+}
