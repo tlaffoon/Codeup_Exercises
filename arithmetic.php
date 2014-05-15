792137742e0b56885189810f3c1e2af1d7d671d6
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-15 09:04:22 -0500

    Added echo statement for output of functions.

--- arithmetic.php
+++ arithmetic.php
@@ -1,19 +1,24 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo ($a + $b) . PHP_EOL ;
 }
 
 function subtract($a, $b) {
-    echo $a - $b;
+    echo ($a - $b)  . PHP_EOL ;
 }
 
 function multiply($a, $b) {
-    echo $a * $b;
+    echo ($a * $b)  . PHP_EOL ;
 }
 
 function divide($a, $b) {
-    echo $a / $b;
+    echo ($a / $b)  . PHP_EOL ;
 }
+
+add(7,4);
+subtract(7,4);
+multiply(7,4);
+divide(7,4);
 
 ?>
