5efcaddbd475d685292d72695cf3c84837ed668a
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-15 09:18:37 -0500

    Added input validation and error messages displaying value of input.

--- arithmetic.php
+++ arithmetic.php
@@ -4,7 +4,7 @@ function add($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
     	echo ($a + $b) . PHP_EOL ;
     } else {
-        echo "ERROR: Both arguments must be numbers\n";
+        echo "ERROR: Both arguments must be numbers. Attempted $a + $b \n";
     }
 }
 
@@ -12,7 +12,7 @@ function subtract($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
     	echo ($a - $b) . PHP_EOL ;
     } else {
-        echo "ERROR: Both arguments must be numbers\n";
+        echo "ERROR: Invalid input. Attempted $a - $b \n";
     }
 }
 
@@ -20,15 +20,15 @@ function multiply($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
     	echo ($a * $b) . PHP_EOL ;
     } else {
-        echo "ERROR: Both arguments must be numbers\n";
+        echo "ERROR: Invalid input. Attempted $a * $b \n";
     }
 }
 
 function divide($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
+	if (is_numeric($a) && is_numeric($b) && $b != 0) {
     	echo ($a / $b) . PHP_EOL ;
     } else {
-        echo "ERROR: Both arguments must be numbers\n";
+        echo "ERROR: Invalid input. Attempted $a / $b \n";
     }
 }
 
@@ -36,14 +36,14 @@ function modulus($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
     	echo ($a % $b) . PHP_EOL ;
     } else {
-        echo "ERROR: Both arguments must be numbers\n";
+        echo "ERROR: Invalid input. Attempted $a % $b \n";
     }
 }
 
 add(7,4);
 subtract(7,4);
 multiply(7,4);
-divide(7,4);
+divide(7,0);
 modulus(10,100);
 
 ?>
