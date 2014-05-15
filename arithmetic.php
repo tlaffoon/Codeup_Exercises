b947e4c69444a039ddc6687f22f041892ae7760d
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-15 15:37:03 -0500

    Modified functions and changed to accept command line arguments.

--- arithmetic.php
+++ arithmetic.php
@@ -1,49 +1,77 @@
 <?php
 
-function add($a, $b) {
+$a = $argv[1];
+$b = $argv[2];
+
+function check($a, $b) {
 	if (is_numeric($a) && is_numeric($b)) {
-    	echo ($a + $b) . PHP_EOL ;
-    } else {
-        echo "ERROR: Both arguments must be numbers. Attempted $a + $b \n";
-    }
+		return true;
+	} else 
+		echo "ERROR: Invalid input. \$a = $a, \$b = $b" . PHP_EOL;
+		return false;
+}
+
+function add($a, $b) {
+	if (check($a, $b)) {
+   	return ($a + $b) . PHP_EOL;
+   }
 }
 
 function subtract($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo ($a - $b) . PHP_EOL ;
-    } else {
-        echo "ERROR: Invalid input. Attempted $a - $b \n";
-    }
+	if (check($a, $b)) {
+    return ($a - $b) . PHP_EOL;
+	}
 }
 
 function multiply($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo ($a * $b) . PHP_EOL ;
-    } else {
-        echo "ERROR: Invalid input. Attempted $a * $b \n";
-    }
+    if (check($a, $b)) {
+    	echo "say what?";
+    return ($a * $b) . PHP_EOL;
+	}
 }
+
+function remainder($a, $b) {
+    if (check($a, $b)) {
+   	return ($a % $b) . PHP_EOL;
+	}
+}	
 
 function divide($a, $b) {
-	if (is_numeric($a) && is_numeric($b) && $b != 0) {
-    	echo ($a / $b) . PHP_EOL ;
-    } else {
-        echo "ERROR: Invalid input. Attempted $a / $b \n";
-    }
+    if (check($a, $b)) {
+    } elseif ($b != 0) {
+      return ($a / $b) . PHP_EOL;
+    } else echo "Man, you aren't trying to divide by zero - are you?" . PHP_EOL;
 }
 
-function modulus($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-    	echo ($a % $b) . PHP_EOL ;
-    } else {
-        echo "ERROR: Invalid input. Attempted $a % $b \n";
-    }
-}
 
-add(7,4);
-subtract(7,4);
-multiply(7,4);
-divide(7,0);
-modulus(10,100);
+
+fwrite(STDOUT, 'A, S, D, M, R' . PHP_EOL);
+$operation = trim(fgets(STDIN));
+
+switch ($operation) {
+	case 'A':
+		echo add($a, $b);
+		break;
+
+	case 'S':
+		subtract($a, $b);
+		break;
+
+	case 'D':
+		divide($a, $b);
+		break;
+
+	case 'M':
+		multiply($a, $b);
+		break;
+
+	case 'R':
+		remainder($a, $b);
+		break;
+	
+	default:
+		echo "default \n";
+		break;
+}
 
 ?>
