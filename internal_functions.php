c6c8da3e59e06fcdab2cfe3f6948228f99b7990e
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-05-16 09:09:13 -0500

    Initial commit of exercise.

---
+++ internal_functions.php
@@ -0,0 +1,22 @@
+<?php
+
+$nothing = NULL;
+
+$something = '';
+
+$array = array(1,2,3);
+foreach ($array as $item) {
+ 	echo $item . PHP_EOL ;
+ }
+ 
+// Create a function that checks if a variable is set or empty, and display "$variable_name is SET|EMPTY"
+
+// TEST: If var $nothing is set, display '$nothing is SET'
+
+// TEST: If var $nothing is empty, display '$nothing is EMPTY'
+
+// TEST: If var $something is set, display '$something is SET'
+
+// Serialize the array $array, and output the results
+
+// Unserialize the array $array, and output the results
